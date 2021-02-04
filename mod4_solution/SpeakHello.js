515c7170f314886470d23baa681506927b8d3317
Italo Rossi <italox@gmail.com>
2021-02-04 17:59:50 -0500

    Assignment 4 - Solution

--- mod4_solution/SpeakHello.js
+++ mod4_solution/SpeakHello.js
@@ -1,24 +1,11 @@
-// STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
-// See Lecture 52, part 2
+(function (window){ // OPENING IIFE
 
+var helloSpeaker = {}; // CREATING EMPTY LITERAL
 
-// STEP 3: Create an object, called 'helloSpeaker' to which you will attach
-// the "speak" method and which you will expose to the global context
-// See Lecture 52, part 1
-// var helloSpeaker =
-
-// DO NOT attach the speakWord variable to the 'helloSpeaker' object.
 var speakWord = "Hello";
 
-// STEP 4: Rewrite the 'speak' function such that it is attached to the
-// helloSpeaker object instead of being a standalone function.
-// See Lecture 52, part 2
-function speak(name) {
-  console.log(speakWord + " " + name);
+helloSpeaker.speak = function(name){ // SPEAK FUNCTION AS PART OF the helloSpeaker LITERAL
+	console.log(speakWord + " " + name);
 }
-
-// STEP 5: Expose the 'helloSpeaker' object to the global scope. Name it
-// 'helloSpeaker' on the global scope as well.
-// See Lecture 52, part 2
-// (Note, Step 6 will be done in the SpeakGoodBye.js file.)
-// xxxx.xxxx = helloSpeaker;
+ window.helloSpeaker = helloSpeaker; // COPYING THE CURRENT ghelloSpeaker VALUE TO THE GLOBAL CONTEXT
+})(window); // CLOSING IIFE
